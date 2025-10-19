cc8df857cb6869742d2a55c8f792ee87d427beaa
Amjad Sharaf <amjdsharfking@gmail.com>
2025-10-19 19:33:48 +0300

    test

--- lib/Screens/Bnb/cart_screen.dart
+++ lib/Screens/Bnb/cart_screen.dart
@@ -8,12 +8,91 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
+  List<Widget> items = List.generate(80, (index) {
+    return Text("Top Teck");
+  });
+  List<Color> colors = [
+    Colors.amber,
+    Colors.red,
+    Colors.green,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.yellowAccent,
+    Colors.pink,
+    Colors.pinkAccent,
+    Colors.amber,
+    Colors.red,
+    Colors.green,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.yellowAccent,
+    Colors.pink,
+    Colors.pinkAccent,
+    Colors.amber,
+    Colors.red,
+    Colors.green,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.yellowAccent,
+    Colors.pink,
+    Colors.pinkAccent,
+    Colors.amber,
+    Colors.red,
+    Colors.green,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.yellowAccent,
+    Colors.pink,
+    Colors.pinkAccent,
+    Colors.amber,
+    Colors.red,
+    Colors.green,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.yellowAccent,
+    Colors.pink,
+    Colors.pinkAccent,
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Cart page"  , style: TextStyle(fontSize: 60 , color: Colors.black),),
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        mainAxisExtent: 100,
+        crossAxisCount: 3,
       ),
+      itemCount: colors.length,
+      itemBuilder: (context, index) {
+        return Container(
+          alignment: Alignment.center,
+          child: items[index],
+          color: colors[index],
+        );
+      },
     );
+
+    //  GridView(
+    //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+    //     crossAxisSpacing: 10,
+    //     mainAxisSpacing: 10,
+    //     mainAxisExtent: 300,
+
+    //     crossAxisCount: 2,
+    //   ),
+    //   children: [
+    //     Container(color: Colors.amber),
+    //     Container(color: Colors.amber),
+    //     Container(color: Colors.amber),
+    //     Container(color: Colors.amber),
+    //     Container(color: Colors.amber),
+    //     Container(color: Colors.amber),
+    //     Container(color: Colors.amber),
+    //     Container(color: Colors.amber),
+    //     Container(color: Colors.amber),
+    //     Container(color: Colors.amber),
+    //     Container(color: Colors.amber),
+    //   ],
+    // );
   }
 }
