45337d6ff7c42b97da727b831a4f97ecbb6db4c4
Amjad Sharaf <amjdsharfking@gmail.com>
2025-10-19 20:08:28 +0300

    test

--- lib/Screens/Bnb/profile_screen.dart
+++ lib/Screens/Bnb/profile_screen.dart
@@ -8,11 +8,106 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  List<Widget> items = List.generate(80, (index) {
+    return Text("Top Teck ${index + 1}", style: TextStyle(fontSize: 30));
+  });
+  List<Color> colors = [
+    Colors.amber,
+    Colors.red,
+    Colors.green,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.yellowAccent,
+    Colors.pink,
+    Colors.pinkAccent,
+    Colors.amber,
+    Colors.red,
+    Colors.green,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.yellowAccent,
+    Colors.pink,
+    Colors.pinkAccent,
+    Colors.amber,
+    Colors.red,
+    Colors.green,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.yellowAccent,
+    Colors.pink,
+    Colors.pinkAccent,
+    Colors.amber,
+    Colors.red,
+    Colors.green,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.yellowAccent,
+    Colors.pink,
+    Colors.pinkAccent,
+    Colors.amber,
+    Colors.red,
+    Colors.green,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.yellowAccent,
+    Colors.pink,
+    Colors.pinkAccent,
+  ];
+  late PageController pageController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    pageController = PageController();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Profile page"  , style: TextStyle(fontSize: 60 , color: Colors.black),),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              InkWell(
+                onTap: () {
+                  pageController.previousPage(
+                    duration: Duration(seconds: 1),
+                    curve: Curves.decelerate,
+                  );
+                },
+                child: Text(
+                  "prov",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  pageController.nextPage(
+                    duration: Duration(seconds: 1),
+                    curve: Curves.bounceIn,
+                  );
+                },
+                child: Text(
+                  "next",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 30),
+          Expanded(
+            child: PageView.builder(
+              controller: pageController,
+              itemCount: colors.length,
+              itemBuilder: (context, index) {
+                return Container(color: colors[index], child: items[index]);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
