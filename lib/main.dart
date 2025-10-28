e157c38c534e7c388c5ca96c2cc098688be4b16b
Amjad Sharaf <amjdsharfking@gmail.com>
2025-10-28 19:45:33 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Mainscreen());
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'palyfair'),
+      debugShowCheckedModeBanner: false,
+      home: Mainscreen(),
+    );
   }
 }
